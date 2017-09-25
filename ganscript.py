54c0c9d57e188755939b8cc9820bb7c9344b50f6
Sam Pringle <15springle@gmail.com>
2017-09-25 10:07:59 -0700

    Turn on sync

--- ganscript.py
+++ ganscript.py
@@ -145,21 +145,21 @@ def main(server, log_dir, context):
 
     # Train the generator
     g_opt = tf.train.AdamOptimizer(g_learning_rate)
-    # g_opt = tf.train.SyncReplicasOptimizer(g_opt, replicas_to_aggregate=num_workers-2,
-    #                                       total_num_replicas=num_workers-1)
-    g_trainer = g_opt.minimize(g_loss, var_list=g_vars, global_step=g_global_step)
+    g_opt = tf.train.SyncReplicasOptimizer(g_opt, replicas_to_aggregate=num_workers,
+                                           total_num_replicas=num_workers)
+    g_trainer = g_opt.minimize(g_loss, var_list=g_vars)  # , global_step=g_global_step)
 
     # Train the fake discriminator
     d_opt_fake = tf.train.AdamOptimizer(d_fake_learning_rate)
-    # d_opt_fake = tf.train.SyncReplicasOptimizer(d_opt_fake, replicas_to_aggregate=num_workers-2,
-    #                                            total_num_replicas=num_workers-1)
-    d_trainer_fake = d_opt_fake.minimize(d_loss_fake, var_list=d_vars, global_step=d_fake_global_step)
+    d_opt_fake = tf.train.SyncReplicasOptimizer(d_opt_fake, replicas_to_aggregate=num_workers,
+                                                total_num_replicas=num_workers)
+    d_trainer_fake = d_opt_fake.minimize(d_loss_fake, var_list=d_vars)  # , global_step=d_fake_global_step)
 
     # Train the real discriminator
     d_opt_real = tf.train.AdamOptimizer(d_real_learning_rate)
-    # d_opt_real = tf.train.SyncReplicasOptimizer(d_opt_real, replicas_to_aggregate=num_workers-2,
-    #                                            total_num_replicas=num_workers-1)
-    d_trainer_real = d_opt_real.minimize(d_loss_real, var_list=d_vars, global_step=d_real_global_step)
+    d_opt_real = tf.train.SyncReplicasOptimizer(d_opt_real, replicas_to_aggregate=num_workers,
+                                                total_num_replicas=num_workers)
+    d_trainer_real = d_opt_real.minimize(d_loss_real, var_list=d_vars)  # , global_step=d_real_global_step)
 
     # From this point forward, reuse variables
     tf.get_variable_scope().reuse_variables()
@@ -174,10 +174,9 @@ def main(server, log_dir, context):
     merged = tf.summary.merge_all()
 
     is_chief = server.server_def.task_index == 0
-    # hooks = [g_opt.make_session_run_hook(is_chief),
-    #          d_opt_fake.make_session_run_hook(is_chief),
-    #          d_opt_real.make_session_run_hook(is_chief)]
-    hooks = []
+    hooks = [g_opt.make_session_run_hook(is_chief),
+             d_opt_fake.make_session_run_hook(is_chief),
+             d_opt_real.make_session_run_hook(is_chief)]
     with tf.train.MonitoredTrainingSession(master=server.target,
                                            is_chief=is_chief,
                                            hooks=hooks) as sess:
@@ -190,7 +189,8 @@ def main(server, log_dir, context):
         local_step = 0
         while sess.run(g_global_step) < 1000000:
             d_fake_step, d_real_step, g_step = sess.run([d_fake_global_step, d_real_global_step, g_global_step])
-            if (d_fake_step < pre_train_steps) and (d_real_step < pre_train_steps):
+            # if (d_fake_step < pre_train_steps) and (d_real_step < pre_train_steps):
+            if local_step < 100:
                 print("[step] pre-training... local_step={}, d_fake_global_step={}, d_real_global_step={}, "
                       "g_global_step={}".format(local_step, d_fake_step, d_real_step, g_step))
                 z_batch = np.random.normal(0, 1, size=[batch_size, z_dimensions])
@@ -217,7 +217,7 @@ def main(server, log_dir, context):
                 print("Saving summary {}".format(str(local_step / 10)))
                 z_batch = np.random.normal(0, 1, size=[batch_size, z_dimensions])
                 summary = sess.run(merged, {z_placeholder: z_batch, x_placeholder: real_image_batch})
-                writer.add_summary(summary, local_step / 10)
+                writer.add_summary(summary, d_fake_step)
 
             if local_step % 50 == 0:
                 print("real training... local_step={}, d_fake_global_step={}, d_real_global_step={}, "
